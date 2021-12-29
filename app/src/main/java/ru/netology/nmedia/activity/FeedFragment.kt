cec325f02c67275c6832d2027be2600fd73ddc3e
Your Name <seregaklim@mail.ru>
2021-12-29 17:14:48 +0300

    first commit

--- app/src/main/java/ru/netology/nmedia/activity/FeedFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/FeedFragment.kt
@@ -19,9 +19,6 @@ import ru.netology.nmedia.databinding.FragmentFeedBinding
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.model.ActionType
 import ru.netology.nmedia.viewmodel.PostViewModel
-
-
-
 class FeedFragment : Fragment() {
 
     private val viewModel: PostViewModel by viewModels(ownerProducer = ::requireParentFragment)
